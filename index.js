1412381528b85893f8698d302a0c50dc5e073c6a
Mariela  Ibañez <ibanezmariela78@gmail.com>
2026-09-21 14:15:03 -0300

    Agrega consulta GET de producto por id

--- index.js
+++ index.js
@@ -18,8 +18,27 @@ async function obtenerProductos() {
     }
 }
 
+async function obtenerProductoPorId(id) {
+    try {
+        const respuesta = await fetch(`https://fakestoreapi.com/products/${id}`);
+
+        if (!respuesta.ok) {
+            throw new Error(`Error en la petición: ${respuesta.status}`);
+        }
+
+        const producto = await respuesta.json();
+
+        console.log(producto);
+    } catch (error) {
+        console.error("Error al obtener el producto:", error.message);
+    }
+}
+
 if (metodo === "GET" && recurso === "products") {
     obtenerProductos();
+} else if (metodo === "GET" && recurso.startsWith("products/")) {
+    const id = recurso.split("/")[1];
+    obtenerProductoPorId(id);
 } else {
     console.log("Comando no reconocido");
 }
