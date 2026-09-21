2d4251dee0d0ed15c6500af86adfdf61e06278fc
Mariela  Ibañez <ibanezmariela78@gmail.com>
2026-09-21 14:09:37 -0300

    Agrega consulta GET de productos con FakeStore

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const argumentos = process.argv.slice(2);
 
-console.log(argumentos);
+const [metodo, recurso] = argumentos;
+
+async function obtenerProductos() {
+    try {
+        const respuesta = await fetch("https://fakestoreapi.com/products");
+
+        if (!respuesta.ok) {
+            throw new Error(`Error en la petición: ${respuesta.status}`);
+        }
+
+        const productos = await respuesta.json();
+
+        console.log(productos);
+    } catch (error) {
+        console.error("Error al obtener los productos:", error.message);
+    }
+}
+
+if (metodo === "GET" && recurso === "products") {
+    obtenerProductos();
+} else {
+    console.log("Comando no reconocido");
+}
